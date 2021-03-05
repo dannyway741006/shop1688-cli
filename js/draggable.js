296bc872b3ce91d070754eb9c531af861286361e
wilson-lin <dannyway741006@gmail.com>
2021-03-06 01:27:10 +0800

    no message

--- js/draggable.js
+++ js/draggable.js
@@ -11,11 +11,7 @@ import {
 gsap.registerPlugin(Draggable);
 // console.log(Draggable);
 // import Vue and vue-parallax-js
-import Vue from 'vue'
-import VueParallaxJs from 'vue-parallax-js'
 
-// add vue-parallax-js to vue
-Vue.use(VueParallaxJs)
 
 
 import SearchBox from "@/components/searchBox.vue";
